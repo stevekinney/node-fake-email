fe7206cd7c6da86c0115bb08089810b0372a46c9
Steve Kinney <hello@stevekinney.net>
2013-03-04 20:11:58 -0500

    Add in some basic tests

---
+++ test/test.js
@@ -0,0 +1,20 @@
+var assert = require('assert');
+var generate = require('../');
+
+describe('Fake Email Address Generator', function() {
+  
+  it('should generate a valid email address', function() {
+    var name = "Steve Kinney";
+    for(var i; i < 10; i++) {
+      assert.ok(!!generate(name).match(/^(.+)@\w+\.\w{3}$/));
+    }
+  });
+  
+  it('should strip out apostrophes from names', function () {
+    var name = "Brian O'Connell"
+    for(var i; i < 20; i++) {
+      assert.ok(!generate(name).match("'"));
+    }
+  });
+  
+});
